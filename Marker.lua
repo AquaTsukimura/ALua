9006f56e077ddced0aeb145e878e22e15f3f3bd8
Aqua Tsukimura <165631517+AquaTsukimura@users.noreply.github.com>
2026-04-25 21:17:57 +0500

    Update Marker.lua

--- Marker.lua
+++ Marker.lua
@@ -2,7 +2,7 @@ Main_Logic_Files = {}
 Main_Client_Files = {}
 Main_Loaded_Files = {}
 
-function mark(USING_NAMESPACE, name, moduleTbl)
+function mark(USING_NAMESPACE, name)
   if USING_NAMESPACE == "USING NAMESPACE LOGIC" then
     Main_Logic_Files[name] = true
   elseif USING_NAMESPACE == "USING NAMESPACE CLIENT" then
@@ -10,4 +10,8 @@ function mark(USING_NAMESPACE, name, moduleTbl)
   elseif USING_NAMESPACE == "USING SAMESPACE LOADED" then
     Main_Loaded_Files[name] = true
   end
+end
+
+return function(namespace, name)
+  mark(namespace, name)
 end
